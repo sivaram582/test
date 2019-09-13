5f7b38fca77965c791bc3d01f00512b6edc948d0
KHAMBAMPATI <KHAMBAMPATI@10.20.40.230>
2019-09-13 12:18:52 -0400

    created the class

---
+++ project 1/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		multiple obj=new multiple();
+		obj.add();
+		obj.div();
+		obj.mul();
+
+	}
+
+}
---
+++ project 1/src/multiple.java
@@ -0,0 +1,26 @@
+
+public class multiple 
+{
+
+	int a=10,b=20,c=30;
+	public int add()
+	{
+		int d=a+b+c;
+		return d;
+		
+	}
+	public int mul()
+	{
+		int d=a*b*c;
+		return d;
+	}
+	public void div()
+	{
+		int d=a/b/c;
+		
+	}
+	
+	
+
+}
+
